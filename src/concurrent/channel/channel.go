da73d1e87def00b0237ce0414c2ce72b518c1c0c
championshipapp <jcutrono@gmail.com>
2016-10-03 00:30:54 -0400

    adding logging to channel example

--- src/concurrent/channel/channel.go
+++ src/concurrent/channel/channel.go
@@ -1,30 +1,53 @@
 package channel
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 var global int = 0
 var c = make(chan int, 1)
 
-func thread1() {
-	fmt.Println("In thread 1")
-	<-c
-	global = 1
-	c <- 1
+func thread1(wg *sync.WaitGroup) {
+
+	msg := <-c // this blocks the channel
+	fmt.Println("In thread 1: ", msg)
+
+	setGlobal(msg)
+	msg++
+	c <- msg
+
+	wg.Done()
 }
 
-func thread2() {
-	fmt.Println("In thread 2")
+func thread2(wg *sync.WaitGroup) {
 
-	<-c
-	global = 2
-	c <- 1
+	msg := <-c // this blocks the channel
+	fmt.Println("In thread 2: ", msg)
+
+	setGlobal(msg)
+	msg++
+	c <- msg
+
+	wg.Done()
+}
+
+func setGlobal(i int) {
+
+	fmt.Println("update global: ", i)
+	global = i
 }
 
 func Run() {
 
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	c <- 1 // Put the initial value into the channel
-	go thread1()
-	go thread2()
+	go thread1(&wg)
+	go thread2(&wg)
+
+	wg.Wait()
 
 	fmt.Println("")
 	fmt.Println("all done")
